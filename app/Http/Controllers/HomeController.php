eab298bda1a65ede04dde6dbab9ffbe3edf37d2c
rakhi <rakhi-azfa@torche-indonesia.com>
2024-01-08 15:25:23 +0700

    Redirect from home to login.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Inertia\Inertia;
-
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Home');
+        return to_route('login');
     }
 }
